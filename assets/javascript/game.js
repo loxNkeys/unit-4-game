3b4df425108905445c06f42e70268e1794238549
loxNkeys <37644631+loxNkeys@users.noreply.github.com>
2018-06-22 23:52:48 -0700

    not complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// to do:
+// 1. make method that upon character selection, the characters not selected 'move' to the 
+// 'Enemies Available To Attack' row
+// 2. make a method that changes the color of the 'border' 
+// 3. make method that, upon clicking thte 'Attack' button, decrements the defender's HP by an increasing amount--use a data 
+// attribute that is updated when the attacker attacks. this value increases until the game
+// is over. the damage dealt to the defender is displayed at the bottom of the defender's pic.
+// 4. make method that upon when the player's hp <= 0 the game ends and everything resets.
+// 5. make method that resets on '.on('click') 
+// 6. make method that when the defender is clicked, the defender 'moves' to the defender row & the color changes.
+
+// $('#one').on('click' function() {
+// 	alert('hi');
+// }
+var characters [
+	{
+		name = jar;
+		hp = 100
+	};
+	{
+		name = c3po;
+		hp = 105
+	}
+	
+]
+
+
+
+	$("#").append($(".availChatacters"));
